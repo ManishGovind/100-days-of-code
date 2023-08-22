bb0fe44d5e7b265ec5d3b390a0dc8431f285a329
Manish_7 <manishgnote69@gmail.com>
2023-08-21 23:15:44 -0400

    Day 38 problems updated

---
+++ Day 38/repeated-subarray.cpp
@@ -0,0 +1,13 @@
+/*
+
+Given a string s, check if it can be constructed by taking a substring of it and appending multiple copies of the substring together.
+*/
+
+class Solution {
+public:
+    bool repeatedSubstringPattern(string s) {
+        string t = s + s;
+        if (t.substr(1, t.size() - 2).find(s) != -1) return true;
+        return false;
+    }
+};
