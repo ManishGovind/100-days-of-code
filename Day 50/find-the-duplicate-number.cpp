1a4517846b24d4b281c653398b7175771ac96f4d
Manish_7 <manishgnote69@gmail.com>
2023-09-20 20:06:58 -0400

    Added Day 50 Problems

---
+++ Day 50/find-the-duplicate-number.cpp
@@ -0,0 +1,19 @@
+/*/
+Given an array of integers nums containing n + 1 integers where each integer is in the range[1, n] inclusive.
+
+There is only one repeated number in nums, return this repeated number.
+
+You must solve the problem without modifying the array nums and uses only constant extra space.
+
+*/
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int i = 0;
+        for (i = 0; i < nums.size() - 1; i++) {
+            if (nums[i] == nums[i + 1]) break;
+        }
+        return nums[i];
+    }
+};
